67bc0f42f0910f9a1784eca286bb4d43f82e6998
Kristin Fritsch <ich@kristin-fritsch.de>
2022-04-23 17:19:29 +0200

    drk cookie banner

--- Extentions/content.js
+++ Extentions/content.js
@@ -8,3 +8,9 @@ chrome.storage.local.set({"cookieCounter": 1});
 chrome.storage.local.get("cookieCounter", function(res) {
   console.log(res)
 });
+
+
+setTimeout(function() {
+  // DRK page click cookie banner
+  document.querySelector("body > div.cc-window.cc-banner.cc-type-opt-in.cc-theme-epx.cc-bottom.cc-color-override-234041873 > div > div.epx-banner-widget-buttons > button.cc-btn.cc-detail-btn").click();
+}, 2000);
