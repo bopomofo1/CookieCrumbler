70ef31006cbd77518849b20a7aa54d2c55d52009
Kristin Fritsch <ich@kristin-fritsch.de>
2022-04-23 17:16:57 +0200

    set and get counter

--- Extentions/content.js
+++ Extentions/content.js
@@ -1,2 +1,10 @@
 // content.js
 alert("CookieCrumbler sagt Hallo")
+
+// set cookie counter var
+chrome.storage.local.set({"cookieCounter": 1});
+
+// get cookie counter var
+chrome.storage.local.get("cookieCounter", function(res) {
+  console.log(res)
+});
