dadb49166cc24901e6eb9739972188799a917977
Florian <bopomo1@protonmail.com>
2022-04-23 14:54:55 +0200

    neu

--- Extentions/background.js
+++ Extentions/background.js
@@ -1,6 +1,10 @@
-let color = '##2596be';
+// background.js
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+  });
---
+++ Extentions/content.js
@@ -0,0 +1,2 @@
+// content.js
+alert("CookieCrumbler sagt Hallo")
