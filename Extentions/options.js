8a9c87606c07b02d09378da99357857c3850eb21
Niklas 2.0 <nniklas.kroenke@web.de>
2022-04-23 12:33:21 +0000

    vier farben

--- Extentions/options.js
+++ Extentions/options.js
@@ -1,6 +1,6 @@
 let page = document.getElementById("buttonDiv");
 let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c"];
+const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
 // Reacts to a button click by marking marking the selected button and saving
 // the selection
