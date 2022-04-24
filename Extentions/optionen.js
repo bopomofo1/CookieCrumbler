1321afd9a878f0f9399796f6e3263e875a507240
Live session user <anchanga2017@gmail.com>
2022-04-24 08:27:04 +0000

    zusammengefügt

--- Extentions/optionen.js
+++ Extentions/optionen.js
@@ -16,3 +16,60 @@ function onNotwendig()
         }
         var checkboxNotwendig = document.getElementById("notwendig");
         checkboxNotwendig.addEventListener('click', onNotwendig);
+
+function onLeistung()
+        {
+        
+            var checkbox=document.getElementById("leistung");
+            if (checkbox.checked==true)
+            {
+                chrome.storage.local.set({"leistungwert": 1});
+            }
+            else 
+            {
+                chrome.storage.local.set({"leistungwert": 0});
+            } 
+                chrome.storage.local.get("leistungwert", function(res) {
+                  console.log(res)
+            });
+        }
+        var checkboxLeistung = document.getElementById("leistung");
+        checkboxLeistung.addEventListener('click', onLeistung);
+
+function onFunktions()
+        {
+        
+            var checkbox=document.getElementById("funktions");
+            if (checkbox.checked==true)
+            {
+                chrome.storage.local.set({"funktionswert": 1});
+            }
+            else 
+            {
+                chrome.storage.local.set({"funktionswert": 0});
+            } 
+                chrome.storage.local.get("funktionswert", function(res) {
+                  console.log(res)
+            });
+        }
+        var checkboxFunktions = document.getElementById("funktions");
+        checkboxFunktions.addEventListener('click', onFunktions);
+
+function onWerbung()
+        {
+        
+            var checkbox=document.getElementById("werbung");
+            if (checkbox.checked==true)
+            {
+                chrome.storage.local.set({"werbungwert": 1});
+            }
+            else 
+            {
+                chrome.storage.local.set({"werbungwert": 0});
+            } 
+                chrome.storage.local.get("werbungwert", function(res) {
+                  console.log(res)
+            });
+        }
+        var checkboxWerbung = document.getElementById("werbung");
+        checkboxWerbung.addEventListener('click', onWerbung);
